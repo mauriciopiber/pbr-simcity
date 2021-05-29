73ded47388e9f6d212787d1f3a89b56e079ffaee
Mauricio Piber <mauriciopiber@gmail.com>
2021-05-29 12:21:13 -0300

    added dockerfile production v1

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1 data-testid="title">Haha</h1>
+      <h1 data-testid="title">Haha hehe</h1>
 
     </div>
   );
